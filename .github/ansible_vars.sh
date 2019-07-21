463d8c1277535e6fc21e9a98dc6a183c43b9a32c
Feng <feng.zh@gmail.com>
2019-07-21 18:50:25 +0800

    support extra vars from url

--- .github/ansible_vars.sh
+++ .github/ansible_vars.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-echo "$EXTRA_VARS" > .extra_vars
+if [ "$EXTRA_VARS" = http* ]; then
+  apt-get update && apt-get install curl -y && apt-get clean -y
+  curl -L -q $EXTRA_VARS > .extra_vars
+else
+  echo "$EXTRA_VARS" > .extra_vars
+fi
