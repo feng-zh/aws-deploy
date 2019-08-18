cb682920558249e5bc9042bdfae88c8e6ce70bac
Feng <feng.zh@gmail.com>
2019-08-19 03:01:46 +0800

    simplified github actions workflow

--- .github/workflows/ansible_vars.sh
+++ .github/workflows/ansible_vars.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [[ "$EXTRA_VARS" = http* ]]; then
-  apt-get update && apt-get install curl -y && apt-get clean -y
   curl -Lqs "$EXTRA_VARS" > .extra_vars
 else
   echo "$EXTRA_VARS" > .extra_vars
